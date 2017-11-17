cf5b0a080661c16ebda65ce0b45508838fd4ad66
LaLaTiao <365719132@qq.com>
2017-11-17 16:21:22 +0800

    自定义View05 ==>交作业

--- app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice01AfterOnDrawView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice01AfterOnDrawView.java
@@ -39,14 +39,26 @@ public class Practice01AfterOnDrawView extends AppCompatImageView {
         // 在 super.onDraw() 的下方插入绘制代码，让绘制内容盖住原主体内容
         // 由于这期的重点是绘制代码的位置而不是绘制代码本身，所以直接给出绘制代码，你只要解除注释就好
         // 爽吧？
-
-        /*Drawable drawable = getDrawable();
+        /**
+         * 顺序是:
+         *              drawBackground()     ==>不可重写,只能在xml配置background属性
+         * ViewGroup    onDraw()
+         * View         onDraw()
+         * ViewGroup    diaspathDraw()
+         * View         dispathDraw()       ==>由于 View 是没有子 View 的，所以一般来说 dispatchDraw() 这个方法只对 ViewGroup（以及它的子类）有意义。
+         *              onDrawForeground
+         *
+         * Draw()是在整合了他们4个方法.
+         * 如果写在super.draw()前面就是写在drawBackground()前面
+         * 如果写在super.draw()后面,那么就是写在onDrawForeground后面.
+         */
+        Drawable drawable = getDrawable();
         if (drawable != null) {
             canvas.save();
             canvas.concat(getImageMatrix());
             Rect bounds = drawable.getBounds();
             canvas.drawText(getResources().getString(R.string.image_size, bounds.width(), bounds.height()), 20, 40, paint);
             canvas.restore();
-        }*/
+        }
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice05AfterOnDrawForegroundView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice05AfterOnDrawForegroundView.java
@@ -30,12 +30,10 @@ public class Practice05AfterOnDrawForegroundView extends AppCompatImageView {
     @Override
     public void onDrawForeground(Canvas canvas) {
         super.onDrawForeground(canvas);
-
-        // 在 super.onDrawForeground() 的下方插入绘制代码，让绘制内容盖住前景
-        // 由于这期的重点是绘制代码的位置而不是绘制代码本身，所以直接给出绘制代码，你只要解除注释就好
-        /*paint.setColor(Color.parseColor("#f44336"));
+        //半透明遮罩加在了xml的foreground属性上,要保证NEW标签能够正常显示,那么必须要在onDrawForeground的super.onDrawForeground()后面
+        paint.setColor(Color.parseColor("#f44336"));
         canvas.drawRect(0, 40, 200, 120, paint);
         paint.setColor(Color.WHITE);
-        canvas.drawText("New", 20, 100, paint);*/
+        canvas.drawText("New", 20, 100, paint);
     }
 }
